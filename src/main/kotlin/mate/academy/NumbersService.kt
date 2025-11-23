22c21a7b13a731ab948f3b791919e5b71d26446a
Bohdan Vorobiei <vbs@ukr.net>
2025-11-23 12:03:10 +0200

    Task completed

--- src/main/kotlin/mate/academy/NumbersService.kt
+++ src/main/kotlin/mate/academy/NumbersService.kt
@@ -1,3 +1,8 @@
 package mate.academy
 
 // provide your solution below
+class NumbersService {
+    fun ArrayList<*>.getSizeMessage(): String {
+        return "The list contains ${this.size} elements."
+    }
+}
